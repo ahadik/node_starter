5c8dcdb49bf7ddd8ed3ff1ee90ea29600b7a4564
ahadik <ahadik@us.ibm.com>
2016-06-07 23:15:38 -0500

    Adding better port setting to Node app

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ var express = require('express'),
 var app = express(),
     appEnv = cfenv.getAppEnv();
 
+if(process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'staging'){
+	app.set('port', process.env.VCAP_APP_PORT || 80);
+}else{
+	app.set('port', 3000);
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 //app.set(path.join('views', __dirname, '/apps'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,6 +26,6 @@ app.get('/', function(req, res){
     res.send('index.html');
 })
 
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
     console.info('Server listening on port ' + this.address().port);
 });
